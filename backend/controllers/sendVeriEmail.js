a3673adb0e2e998e77d522ff593659a131dac236
BR-64 <smooot.stu@gmail.com>
2025-06-18 07:48:05 +0700

    add veriemail completed

--- backend/controllers/sendVeriEmail.js
+++ backend/controllers/sendVeriEmail.js
@@ -1,15 +1,18 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS.replace(/\s/g, ''),
   },
+  logger: true,
+  debug: true,
 });
 
 async function sendVerificationEmail(email, token) {
-  const url = `${process.env.BASE_URL}/verify-email/${token}`;
+  const url = `${process.env.BASE_URL}/api/auth/verify-email/${token}`;
   await transporter.sendMail({
     from: `"Resto App" <${process.env.EMAIL_USER}>`,
     to: email,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,5 @@
 // routes/auth.js
 const express = require('express');
-const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 const sendVerificationEmail = require('../controllers/sendVeriEmail');
 require('dotenv').config();
@@ -26,7 +25,7 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/signup', async (req, res) => {
-  console.log(req);
+  // console.log(req);
   const { email, password } = req.body;
 
   try {
@@ -39,18 +38,38 @@ router.post('/signup', async (req, res) => {
     // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    // verification token
+    const token = uuidv4();
+
     // create new user
     const newUser = new User({
       email,
       password: hashedPassword,
+      verificationToken: token,
     });
 
     await newUser.save();
+    await sendVerificationEmail(email, token);
 
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({
+      message: 'User created successfully, check you email to verify',
+    });
   } catch (err) {
     res.status(500).json({ message: 'server error', error: err.message });
   }
+});
+
+router.get('/verify-email/:token', async (req, res) => {
+  const { token } = req.params;
+  const user = await User.findOne({ verificationToken: token });
+
+  if (!user) return res.status(400).send('Invalid token');
+
+  user.verified = true;
+  user.verificationToken = null;
+  await user.save();
+
+  res.send('Email verified successfully!');
 });
 
 module.exports = router;
