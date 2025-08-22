ae0f348276ce3cc837d708ca350645bc46c8980c
BR-64 <smooot.stu@gmail.com>
2025-08-22 10:21:23 +0700

    add corsoption2

--- config/cors.js
+++ config/cors.js
@@ -1,9 +1,17 @@
 const allowedOrigins = [
-  process.env.CLIENT_URL || 'http://localhost:5173', // vite default dev server
+  'http://localhost:5173', // vite dev
+  process.env.CLIENT_URL,
 ];
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  // origin: process.env.CLIENT_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS: ' + origin));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
